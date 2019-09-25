1348c55fe19be4d8e9d86e107a65c2693b296d17
Emilio Bondioli <e.bondioli@todo.to.it>
2019-09-25 15:41:28 +0200

    add image loaders and processing

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,97 +1,107 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackInlineSVGPlugin = require('html-webpack-inline-svg-plugin');
+
 require('dotenv').config()
 
 module.exports = {
-    devtool: 'eval-cheap-module-source-map',
-    entry: './src/index.js',
-    devServer: {
-        port: 8080,
-        contentBase: path.join(__dirname, "dist")
-    },
-    node: {
-        fs: 'empty'
-    },
-    resolve: {
-        alias: {
-            modules: path.resolve(__dirname, 'src/modules'),
-            utils: path.resolve(__dirname, 'src/utils'),
-            '~': path.resolve(__dirname, 'src')
+  devtool: 'eval-cheap-module-source-map',
+  entry: './index.js',
+  context: path.resolve(__dirname, 'src'),
+  devServer: {
+    port: 8080,
+    contentBase: path.join(__dirname, "dist")
+  },
+  node: {
+    fs: 'empty'
+  },
+  resolve: {
+    alias: {
+      modules: path.resolve(__dirname, 'src/modules'),
+      utils: path.resolve(__dirname, 'src/utils'),
+      assets: path.resolve(__dirname, 'src/assets'),
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['env'],
+          plugins: ["transform-object-rest-spread"]
         }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                options: {
-                    presets: ['env'],
-                    plugins: ["transform-object-rest-spread"]
-                }
-            },
-            {
-                test: /\.(scss|css)$/,
-                use: [
-                    {
-                        // creates style nodes from JS strings
-                        loader: "style-loader",
-                        options: {
-                            sourceMap: true
-                        }
-                    },
-                    {
-                        // translates CSS into CommonJS
-                        loader: "css-loader",
-                        options: {
-                            sourceMap: true
-                        }
-                    },
-                    {
-                        // compiles Sass to CSS
-                        loader: "sass-loader",
-                        options: {
-                            outputStyle: 'expanded',
-                            sourceMap: true,
-                            sourceMapContents: true
-                        }
-                    }
-                    // Please note we are not running postcss here
-                ]
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          {
+            // creates style nodes from JS strings
+            loader: "style-loader",
+            options: {
+              sourceMap: true
             }
-            ,
-            {
-                // Load all images as base64 encoding if they are smaller than 8192 bytes
-                test: /\.(png|jpg|gif)$/,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            // On development we want to see where the file is coming from, hence we preserve the [path]
-                            name: '[path][name].[ext]?hash=[hash:20]',
-                            limit: 8192
-                        }
-                    }
-                ]
+          },
+          {
+            // translates CSS into CommonJS
+            loader: "css-loader",
+            options: {
+              sourceMap: true
             }
-            ,
-            {
-                // Load all icons
-                test: /\.(eot|woff|woff2|svg|ttf)([\?]?.*)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                    }
-                ]
+          },
+          {
+            // compiles Sass to CSS
+            loader: "sass-loader",
+            options: {
+              outputStyle: 'expanded',
+              sourceMap: true,
+              sourceMapContents: true
             }
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './index.html',
-            inject: true
-        }),
-        new webpack.EnvironmentPlugin([])
-    ]
+          }
+          // Please note we are not running postcss here
+        ]
+      },
+      {
+        test: /\.(html)$/,
+        use: {
+          loader: 'html-loader',
+          options: {
+            attrs: ['img:src']
+          }
+        }
+      },
+      {
+        // Load all images as base64 encoding if they are smaller than 8192 bytes
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: './assets/img'
+            }
+          }
+        ]
+      },
+      {
+        // Load all icons
+        test: /\.(eot|woff|woff2|ttf)([\?]?.*)$/,
+        use: [
+          {
+            loader: 'file-loader',
+          }
+        ]
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      inject: true
+    }),
+    new HtmlWebpackInlineSVGPlugin(),
+    new webpack.EnvironmentPlugin([])
+  ]
 };
